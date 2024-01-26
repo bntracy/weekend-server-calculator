20de8122a99584fcc06cabe0a3b61027466dc4a2
Beth Tracy <bntracy@gmail.com>
2024-01-26 16:58:14 -0600

    GET request gets dummy data

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,19 @@
 console.log('client.js is sourced!');
+
+onReady();
+
+function onReady() {
+    getCalculations();
+}
+
+function getCalculations() {
+    axios({
+        method: 'GET',
+        url: '/calculations'
+    }).then(function (response){
+        console.log('Response is good', response);
+        // TODO: put the result on the DOM
+    }).catch(function (error){
+        console.log('error', error);
+    });
+}
--- server/server.js
+++ server/server.js
@@ -9,10 +9,17 @@ app.use(express.static('server/public'));
 // calculation objects:
 let calculations = []
 
+calculations.push({numOne: 3, numTwo: 5, operator: '+', result: 8});
+calculations.push({numOne: 11, numTwo: 7, operator: '-', result: 4});
+
 
 // Here's a wonderful place to make some routes:
 
 // GET /calculations
+
+app.get('/calculations', function(req, res){
+  res.send(calculations);
+});
 
 // POST /calculations
 
