e993b21cabaae175552fb67866e3ea815d873a5f
Beth Tracy <bntracy@gmail.com>
2024-01-26 17:20:07 -0600

    most recent result displays

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -18,6 +18,11 @@ function getCalculations() {
         for (let eachCalc of calculationHistory) {
             document.getElementById('resultHistory').innerHTML += `<li>${eachCalc.numOne} ${eachCalc.operator} ${eachCalc.numTwo} = ${eachCalc.result}</li>`;
         }
+        // display the most recent result, if there is one
+        if (calculationHistory.length) {
+            let mostRecent = calculationHistory[calculationHistory.length - 1];
+            document.getElementById('recentResult').innerHTML = `<h2>${mostRecent.result}</h2>`;
+        }
     }).catch(function (error){
         console.log('error', error);
     });
